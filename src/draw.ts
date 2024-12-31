3319b1720e5b18182c13098bae2a6441913a224f
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2024-12-30 17:52:47 -0700

    refactor

--- src/draw.ts
+++ src/draw.ts
@@ -1,21 +1,29 @@
 import { Circle } from "./linear/circle";
 import { Vector2 } from "./linear/vector2";
 
-export function drawCircle(ctx: CanvasRenderingContext2D, c: Circle) {
+export function drawCircle(ctx: CanvasRenderingContext2D, c: Circle, strokeStyle: string = "#4800ff", fillStyle: string = "#8c3bfe60", lineWidth: number = 2) {
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = fillStyle;
     ctx.beginPath();
     ctx.ellipse(c.center.x, c.center.y, c.radius, c.radius, 0, 0, Math.PI * 2);
     ctx.fill();
     ctx.stroke();
 }
 
-export function drawLine(ctx: CanvasRenderingContext2D, a: Vector2, b: Vector2) {
+export function drawLine(ctx: CanvasRenderingContext2D, a: Vector2, b: Vector2, strokeStyle: string = "#ff55", lineWidth: number = 2) {
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
     ctx.beginPath();
     ctx.moveTo(a.x, a.y);
     ctx.lineTo(b.x, b.y);
     ctx.stroke();
 }
 
-export function drawPoint(ctx: CanvasRenderingContext2D, p: Vector2) {
+export function drawPoint(ctx: CanvasRenderingContext2D, p: Vector2, strokeStyle: string, fillStyle: string, lineWidth: number = 2) {
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = fillStyle;
     ctx.fillRect(p.x - 2, p.y - 2, 4, 4);
     ctx.strokeRect(p.x - 2, p.y - 2, 4, 4);
 }
--- src/main.ts
+++ src/main.ts
@@ -11,166 +11,161 @@ import { solveSpring, Spring } from "./linear/spring";
 const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 export function gameLoop() {
-    const circles: Particle[] = (range(10).map<Circle>(x => ({
-        center: { x: Math.random() * canvas.width, y: Math.random() * canvas.height },
-        radius: 10 + Math.random() * 100
+  const circles: Particle[] = (range(10).map<Circle>(x => ({
+    center: { x: Math.random() * canvas.width, y: Math.random() * canvas.height },
+    radius: 10 + Math.random() * 100
 
-    }))).map<Particle>(circle => ({ ...circle, oldCenter: circle.center }));
+  }))).map<Particle>(circle => ({ ...circle, oldCenter: circle.center }));
 
-    let springs: Spring[] = [];
+  let springs: Spring[] = [];
 
-    let selectedCircle: Particle | null = null;
-    let dragging = false;
-    let offset = { x: 0, y: 0 };
-    let shiftPressed = false;
-    let paused = false;
-    let mouseDownPos: Vector2 = { x: 0, y: 0 };
-    let mousePos: Vector2 = { x: 0, y: 0 };
+  let selectedCircle: Particle | null = null;
+  let dragging = false;
+  let offset = { x: 0, y: 0 };
+  let shiftPressed = false;
+  let paused = false;
+  let mouseDownPos: Vector2 = { x: 0, y: 0 };
+  let mousePos: Vector2 = { x: 0, y: 0 };
 
-    //select a circle on mouse down
-    canvas.addEventListener("mousedown", e => {
-        const mouse = { x: e.offsetX, y: e.offsetY };
-        mouseDownPos = mouse;
-        const lastSelectedCircle = selectedCircle;
-        selectedCircle = null;
+  //select a circle on mouse down
+  canvas.addEventListener("mousedown", e => {
+    const mouse = { x: e.offsetX, y: e.offsetY };
+    mouseDownPos = mouse;
+    const lastSelectedCircle = selectedCircle;
+    selectedCircle = null;
 
-        dragging = true; 
-        for (const circle of circles) {
-            if (collideCirclePoint(circle, mouse)) {
-                // right click to delete
-                if (e.button === 2) {
-                    circles.splice(circles.indexOf(circle), 1);
-                    springs = springs.filter(spring => spring.a !== circle && spring.b !== circle);
-                    dragging = false;
-                    return;
-                } 
-                if (shiftPressed && lastSelectedCircle) { 
-                    if (springs.some(spring => spring.a === selectedCircle && spring.b === circle)) {
-                        springs = springs.filter(spring => spring.a !== selectedCircle || spring.b !== circle);
-                    } else {
-                        springs.push({ a: lastSelectedCircle, b: circle, distance: length(sub(lastSelectedCircle.center, circle.center)) });
-                    }
-                }
-                selectedCircle = circle;
-                offset = { x: mouse.x - circle.center.x, y: mouse.y - circle.center.y };
-            }
+    dragging = true;
+    for (const circle of circles) {
+      if (collideCirclePoint(circle, mouse)) {
+        // right click to delete
+        if (e.button === 2) {
+          circles.splice(circles.indexOf(circle), 1);
+          springs = springs.filter(spring => spring.a !== circle && spring.b !== circle);
+          dragging = false;
+          return;
         }
-
-        if(selectedCircle == null) {
-            circles.push({
-                center: mouse,
-                radius: 10,
-                oldCenter: mouse
-            })
+        if (shiftPressed && lastSelectedCircle) {
+          if (springs.some(spring => spring.a === selectedCircle && spring.b === circle)) {
+            springs = springs.filter(spring => spring.a !== selectedCircle || spring.b !== circle);
+          } else {
+            springs.push({ a: lastSelectedCircle, b: circle, distance: length(sub(lastSelectedCircle.center, circle.center)) });
+          }
         }
-    });
-
-    //deselect a circle on mouse up
-    canvas.addEventListener("mouseup", e => {
-        dragging = false;
-    });
-
-    //move the circle on mouse move
-    canvas.addEventListener("mousemove", e => {
-        mousePos = { x: e.offsetX, y: e.offsetY };
-        if (dragging && selectedCircle) {
-            selectedCircle.oldCenter = selectedCircle.center;
-            selectedCircle.center = { x: e.offsetX - offset.x, y: e.offsetY - offset.y };
-        } else if (dragging) {
-            circles[circles.length - 1].radius = Math.max(length(sub(mouseDownPos, { x: e.offsetX, y: e.offsetY })) , 10) ;
-        }
-    });
-
-    addEventListener("keydown", e => {
-        if (e.key === " ") {
-            paused = !paused;
-        }
-        if (e.key === "Shift") {
-            shiftPressed = true;
-        }
-        // R to delete all:
-        if (e.key === "r") {
-            circles.length = 0;
-            springs.length = 0;
-        }
-    });
-
-    addEventListener("keyup", e => {
-        if (e.key === "Shift") {
-            shiftPressed = false;
-        }
-    });
-
-
-    function iteration() {
-        // fill a white rectangle to clear all:
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-
-        // draw a circle in the middle of the screen:
-        
-
-        // set stroke width and color;
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "#4800ff";
-
-        for (const circle of circles) {
-            ctx.fillStyle = "#8c3bfe60";
-            if (selectedCircle === circle) {
-                ctx.strokeStyle = "#ff0000";
-            } else if ( collideCirclePoint(circle, mousePos)) {
-                ctx.fillStyle = "#00604060";
-                ctx.strokeStyle = "#006040";
-            } else {
-                ctx.strokeStyle = "#4800ff";
-            }
-            drawCircle(ctx, circle);
-        }
-
-        for (const spring of springs) {
-            ctx.strokeStyle = "#ff55 00";
-            ctx.lineWidth = 2;
-            drawLine(ctx, spring.a.center, spring.b.center);
-        }
-
-        const container: Box2 = {
-            min: { x: 0, y: 0 },
-            max: { x: canvas.width, y: canvas.height }
-        }
-
-        // circle circle collisions:
-        if (!paused) {
-            for (const s of springs) {
-                applyCircleCircleCollision(s.a, s.b, solveSpring(s));
-            }
-            for (let i = 0; i < circles.length; i++) {
-                if (selectedCircle !== circles[i] || !dragging) {
-                    verletIntegrate(circles[i], { x: 0, y: 1 }, 1);
-                }
-
-                for (let j = i + 1; j < circles.length; j++) {
-                    const circle1 = circles[i];
-                    const circle2 = circles[j];
-
-                    const collide = collideCircleCircleCheck(circle1, circle2);
-                    if (collide) {
-                        const response = collideCircleCircle(circle1, circle2);
-                        applyCircleCircleCollision(circle1, circle2, response);
-                    }
-                }
-
-                // circle container collisions:
-                circles[i].center = add(circles[i].center, collideCircleContainer(circles[i], container));
-            }
-        }
-
-
-
-        requestAnimationFrame(iteration);
+        selectedCircle = circle;
+        offset = { x: mouse.x - circle.center.x, y: mouse.y - circle.center.y };
+      }
     }
 
+    if (selectedCircle == null) {
+      circles.push({
+        center: mouse,
+        radius: 10,
+        oldCenter: mouse
+      })
+    }
+  });
+
+  //deselect a circle on mouse up
+  canvas.addEventListener("mouseup", e => {
+    dragging = false;
+  });
+
+  //move the circle on mouse move
+  canvas.addEventListener("mousemove", e => {
+    mousePos = { x: e.offsetX, y: e.offsetY };
+    if (dragging && selectedCircle) {
+      selectedCircle.oldCenter = selectedCircle.center;
+      selectedCircle.center = { x: e.offsetX - offset.x, y: e.offsetY - offset.y };
+    } else if (dragging) {
+      circles[circles.length - 1].radius = Math.max(length(sub(mouseDownPos, { x: e.offsetX, y: e.offsetY })), 10);
+    }
+  });
+
+  addEventListener("keydown", e => {
+    if (e.key === " ") {
+      paused = !paused;
+    }
+    if (e.key === "Shift") {
+      shiftPressed = true;
+    }
+    // R to delete all:
+    if (e.key === "r") {
+      circles.length = 0;
+      springs.length = 0;
+    }
+  });
+
+  addEventListener("keyup", e => {
+    if (e.key === "Shift") {
+      shiftPressed = false;
+    }
+  });
+
+  const container: Box2 = {
+    min: { x: 0, y: 0 },
+    max: { x: canvas.width, y: canvas.height }
+  }
+
+
+  function render() {
+    // fill a white rectangle to clear all:
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // set stroke width and color;
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "#4800ff";
+
+    for (const circle of circles) {
+      drawCircle(ctx, circle,
+        selectedCircle === circle ? "#ff0000" :
+          collideCirclePoint(circle, mousePos) ? "#006040" : "#4800ff",
+        collideCirclePoint(circle, mousePos) ? "#00604060" : "#8c3bfe60"
+      );
+    }
+
+    for (const spring of springs) {
+      drawLine(ctx, spring.a.center, spring.b.center);
+    }
+  }
+
+  function simulate() {
+    for (const s of springs) {
+      applyCircleCircleCollision(s.a, s.b, solveSpring(s));
+    }
+    for (let i = 0; i < circles.length; i++) {
+      if (selectedCircle !== circles[i] || !dragging) {
+        verletIntegrate(circles[i], { x: 0, y: 1 }, 0.7);
+      }
+
+      for (let j = i + 1; j < circles.length; j++) {
+        const circle1 = circles[i];
+        const circle2 = circles[j];
+
+        const collide = collideCircleCircleCheck(circle1, circle2);
+        if (collide) {
+          const response = collideCircleCircle(circle1, circle2);
+          applyCircleCircleCollision(circle1, circle2, response);
+        }
+      }
+
+      // circle container collisions:
+      circles[i].center = add(circles[i].center, collideCircleContainer(circles[i], container));
+    }
+  }
+
+  function iteration() {
+    // circle circle collisions:
+    if (!paused) {
+      simulate();
+    }
+
+    render();
+
     requestAnimationFrame(iteration);
+  }
+
+  requestAnimationFrame(iteration);
 }
 
 gameLoop();
